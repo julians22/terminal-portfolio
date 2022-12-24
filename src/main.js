063deb450272fd087b0e0e5e15ad3ac95f85e149
Dean Abner Julian <dabnerjulian@yahoo.com>
2022-12-24 16:35:33 +0700

    remove console env

--- src/main.js
+++ src/main.js
@@ -16,8 +16,6 @@ app.use(router);
 
 const google_measurement = import.meta.env.VITE_GOOGLE_MEASURMENT;
 
-console.log(google_measurement);
-
 app.use(VueGtag, {
     property: {
         id: google_measurement
