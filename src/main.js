f95960d76e5f87be85f036cfbf99c8e294683a41
Dean Abner Julian <dabnerjulian@yahoo.com>
2022-12-24 16:32:06 +0700

    update

--- src/main.js
+++ src/main.js
@@ -14,9 +14,13 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 
+const google_measurement = import.meta.env.VITE_GOOGLE_MEASURMENT;
+
+console.log(google_measurement);
+
 app.use(VueGtag, {
     property: {
-        id: "G-FSNV6HW00B"
+        id: google_measurement
     }
 })
 
